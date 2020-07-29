a9324d4138536954bb2459ed45d80f78e04d6047
Dylan Feng <dfeng102938@gmail.com>
2020-07-28 22:32:03 -0700

    Fixing driver python file

--- driver.py
+++ driver.py
@@ -5,22 +5,20 @@ import csv
 
 
 def main():
-    while True:
-        time.sleep(300)
-        scraper.main()
+    scraper.main()
 
-        try:
-            file = open("output.csv")
-            reader = csv.reader(file)
+    try:
+        file = open("output.csv")
+        reader = csv.reader(file)
 
-            if len(list(reader)) > 1:
-                send_results.main()
-                print("Sent results")
-            else:
-                print("No results to send")
+        if len(list(reader)) > 1:
+            send_results.main()
+            print("Sent results")
+        else:
+            print("No results to send")
 
-        except:
-            pass
+    except:
+        pass
 
 
 if __name__ == "__main__":
--- test.py
+++
@@ -1,3 +0,0 @@
-#!C:\Users\dylan\AppData\Local\Programs\Python\Python38-32\ python
-
-print("Wassup")
